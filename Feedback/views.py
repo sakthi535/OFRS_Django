1e8c1fb1e05f510860873733b9fb8df6c82a7573
sakthi535 <ssakthi20030305@gmail.con>
2022-03-03 14:10:12 +0530

    Initialiazed

---
+++ Feedback/views.py
@@ -0,0 +1,15 @@
+from django.views.generic import TemplateView, CreateView
+from .models import FeedbackData
+
+
+class GetFeedbackView(CreateView):
+    model = FeedbackData
+    fields = '__all__'
+    template_name = 'Feedback/feedback_form.html'
+
+
+class SuccessView(TemplateView):
+    template_name = 'Feedback/success.html'
+
+class ShowTeachersView(TemplateView):
+    template_name = 'Review/list_feedback.html'
