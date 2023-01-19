d2f0bcea67bb046db70848841d66f7ade7e4bc91
sakthi535 <ssakthi20030305@gmail.com>
2023-01-19 21:48:36 +0530

    msg from 4

--- Review/urls.py
+++ Review/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import ListFeedbackView, SearchResultsView, deleteAllFeedback, FeedbackDetailView, DeleteFeedbackView
 
+# comment here
+
 urlpatterns = [
     path('', ListFeedbackView.as_view(), name='ListFeedbackView'),
     path('search/', SearchResultsView.as_view(), name='SearchResultsView'),
